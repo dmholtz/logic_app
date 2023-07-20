fab68626b973dd5001544ec91b3f7631695c6b53
dmholtz <56723830+dmholtz@users.noreply.github.com>
2023-07-20 15:28:31 +0200

    Clean competition screen

--- lib/screens/competition.dart
+++ lib/screens/competition.dart
@@ -19,23 +19,36 @@ class CompetitionScreen extends ConsumerWidget {
         ],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                context
-                    .goNamed('quiz', queryParameters: {"mode": "competition"});
-              },
-              child: const Text("Start"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                context.push('/competition/leaderboard');
-              },
-              child: const Text("Leaderboard"),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "You only have limited time to answer each question. Are you ready?",
+                style: Theme.of(context).textTheme.bodyLarge,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {
+                    context.goNamed('quiz',
+                        queryParameters: {"quiz_mode": "competition"});
+                  },
+                  // create a circular elevated button:
+                  // Source: https://www.fluttercampus.com/guide/290/border-radius-circular-elevated-button/
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                  child: const Text("Start"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
